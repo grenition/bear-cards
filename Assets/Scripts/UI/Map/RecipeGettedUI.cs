500b37f6286204fc427780a702ff8e34ee4203e0
Arssseniy <88106737+grenition@users.noreply.github.com>
2024-12-17 01:56:29 +0300

    feat: UIRecipeGetted panel

---
+++ Assets/Scripts/UI/Map/RecipeGettedUI.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Project
+{
+    public class RecipeGettedUI : MapPanelUI
+    {
+        public override void InteractComplited()
+        {
+            // TODO: give recipe here
+        }
+    }
+}
