52ceaff01b3ef28c9e37587beb5cf74ab74feef9
_keo666_ <egor.master.16@mail.ru>
2024-12-17 08:00:16 +0300

    fix: path interactive points

--- Assets/Scripts/Map/InteractivePoints/Base/PointOfInterestPath.cs
+++ Assets/Scripts/Map/InteractivePoints/Base/PointOfInterestPath.cs
@@ -8,16 +8,7 @@ namespace Assets.Scripts.Map
         public void CreatePath(ViewPoint point)
         {
             _lineRenderer.SetPosition(0, transform.position);
-            Vector3 meadlePoint = point.transform.position;
-
-            if ((transform.position.x - point.transform.position.x) != 0)
-            {
-                float meadlePointX = point.transform.position.x;
-                meadlePoint = new Vector3(transform.position.x, point.transform.position.y, meadlePoint.z);
-            }
-
-            _lineRenderer.SetPosition(1, meadlePoint);
-            _lineRenderer.SetPosition(2, point.transform.position);
+            _lineRenderer.SetPosition(1, point.transform.position);
         }
     }
 }
